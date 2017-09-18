32d14da8aacaa597fc9989a4c5eed7e75278cee0
localadmin <rkunu@allstate.com>
2017-09-18 18:25:59 +0530

    taking todays pull

--- app/src/main/java/com/example/localadmin/toiletsamadhan/RetrofitMaps.java
+++ app/src/main/java/com/example/localadmin/toiletsamadhan/RetrofitMaps.java
@@ -15,7 +15,7 @@ public interface RetrofitMaps {
      * Retrofit get annotation with our URL
      * And our method that will return us details of student.
      */
-    @GET("api/directions/json?key=AIzaSyC22GfkHu9FdgT9SwdCWMwKX1a4aohGifM")
+    @GET("api/directions/json?key=AIzaSyCbbA3qVBiAhWiDQsNRXKfXZd6xJyaxuoA")
     Call<Example> getDistanceDuration(@Query("units") String units, @Query("origin") String origin, @Query("destination") String destination, @Query("mode") String mode);
 
 }
